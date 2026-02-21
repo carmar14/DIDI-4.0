7867a6a9c621d1935e366e6d4e81d71a492b098a
carmar14 <carmar141414@hotmail.com>
2026-02-21 12:23:28 -0500

    Ejemplo de VLM

---
+++ Transformers/ejemplo_VLM.py
@@ -0,0 +1,17 @@
+from transformers import BlipProcessor, BlipForQuestionAnswering
+from PIL import Image
+import torch
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
+
+image = Image.open("ejemplo.jpg").convert("RGB")
+
+question = "How many people are in the image?"
+
+inputs = processor(image, question, return_tensors="pt")
+
+out = model.generate(**inputs)
+answer = processor.decode(out[0], skip_special_tokens=True)
+
+print("Respuesta:", answer)
